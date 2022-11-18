c12173e92d29609e37a22a3d711d1109b79910e6
Brandon Lyons <brandonl.4231@gmail.com>
2022-11-17 19:22:41 -0800

    delete solutions on main branch

--- algos/mergesort.js
+++ algos/mergesort.js
@@ -4,56 +4,6 @@ function swap(arr, i, j) {
   arr[j] = tmp;
 }
 
-function merge(arr, left, middle, right) {
-  const l1 = middle - left + 1;
-  const l2 = right - middle;
+function merge(arr, left, middle, right) {}
 
-  const lArr = new Array(l1);
-  const rArr = new Array(l2);
-
-  for (let i = 0; i < l1; i++) {
-    lArr[i] = arr[left + i];
-  }
-  for (let i = 0; i < l2; i++) {
-    rArr[i] = arr[middle + 1 + i];
-  }
-
-  let i = 0,
-    j = 0,
-    k = left;
-
-  while (i < l1 && j < l2) {
-    if (lArr[i] <= rArr[j]) {
-      arr[k] = lArr[i];
-      i++;
-    } else {
-      arr[k] = rArr[j];
-      j++;
-    }
-    k++;
-  }
-
-  while (i < l1) {
-    arr[k] = lArr[i];
-    i++;
-    k++;
-  }
-
-  while (j < l2) {
-    arr[k] = rArr[j];
-    j++;
-    k++;
-  }
-}
-
-export default function mergeSort(arr, left, right) {
-  if (left >= right) {
-    return;
-  }
-  const middle = left + parseInt((right - left) / 2);
-  mergeSort(arr, left, middle);
-  mergeSort(arr, middle + 1, right);
-  merge(arr, left, middle, right);
-
-  return arr;
-}
+export default function mergeSort(arr, left, right) {}
--- data-structures/heap.js
+++ data-structures/heap.js
@@ -1,74 +1,26 @@
 class BinaryHeap {
-  constructor(arr) {
-    this.heapArr = arr;
-    this.size = this.heapArr.length;
-    this.heapify();
-  }
+  constructor(arr) {}
 
   // Swaps element at index i with element at index j
-  swap(i, j) {
-    const tmp = this.heapArr[i];
-    this.heapArr[i] = this.heapArr[j];
-    this.heapArr[j] = tmp;
-  }
+  swap(i, j) {}
 
   // Returns index of left child of root
-  left(root) {
-    return 2 * root + 1;
-  }
+  left(root) {}
 
   // Returns index of right child of root
-  right(root) {
-    return 2 * root + 2;
-  }
+  right(root) {}
 
   // Returns index of parent element for subtree root
-  parent(root) {
-    if (root === 0) return null;
-
-    return Math.floor((root - 1) / 2);
-  }
+  parent(root) {}
 
   // Peeks root element without removing it
-  peek() {
-    return this.heapArr[0];
-  }
+  peek() {}
 
-  insert(val) {
-    this.heapArr.push(val);
-    this.heapify();
-  }
+  insert(val) {}
 
-  delete(val) {
-    const idx = this.heapArr.indexOf(val);
-    if (idx === undefined) return;
+  delete(val) {}
 
-    if (idx !== this.heapArr.length - 1) {
-      this.swap(idx, this.heapArr.length - 1);
-    }
-    this.heapArr.pop();
-    this.heapify();
-  }
-
-  heapify(i) {
-    // Allows us to default i for the first recursive call
-    if (i === undefined) i = Math.ceil(this.heapArr.length / 2 - 1);
-    // Base case, prevent recursive loop
-    if (i < 0) return;
-
-    let largest = i;
-    let left = this.left(i);
-    let right = this.right(i);
-
-    if (left < this.heapArr.length && this.compare(left, i)) largest = left;
-    if (right < this.heapArr.length && this.compare(right, largest))
-      largest = right;
-
-    // Only swap if a larger/smaller element was found
-    if (largest !== i) this.swap(i, largest);
-
-    this.heapify(i - 1);
-  }
+  heapify(i) {}
 }
 
 export class MaxBinaryHeap extends BinaryHeap {
@@ -76,9 +28,7 @@ export class MaxBinaryHeap extends BinaryHeap {
     super(arr);
   }
 
-  compare(child, root) {
-    return this.heapArr[child] > this.heapArr[root];
-  }
+  compare(child, root) {}
 }
 
 export class MinBinaryHeap extends BinaryHeap {
@@ -86,7 +36,5 @@ export class MinBinaryHeap extends BinaryHeap {
     super(arr);
   }
 
-  compare(child, root) {
-    return this.heapArr[child] < this.heapArr[root];
-  }
+  compare(child, root) {}
 }
